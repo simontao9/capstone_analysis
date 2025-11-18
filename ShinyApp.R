009401f2b0e21086c68a44b4be2487700c49a594
simontao9 <156364678+simontao9@users.noreply.github.com>
2025-11-18 16:02:31 -0500

    Update ShinyApp.R

--- ShinyApp.R
+++ ShinyApp.R
@@ -7,6 +7,8 @@ ui <- fluidPage(
     tabPanel("Tab 2 Title", h2("Content for Tab 2")),
     tabPanel("Tab 3 Title", h2("Content for Tab 3")),
     tabPanel("Tab 4 Title", h2("Content for Tab 4")),
+    tabPanel("Tab 5 Title", h2("Content for Tab 5")),
+    tabPanel("Tab 6 Title", h2("Content for Tab 6")),
   )
 )
 
@@ -14,3 +16,4 @@ server <- function(input, output, session) {
 }
 
 shinyApp(ui=ui, server=server)
+
